c8687c7c025cdc72a1c3dfacac91f4499aa1e14e
KilHyeon <rlaarlfgus@gmail.com>
2022-02-17 23:51:34 +0900

    Create Boards Service.. (*--no-spec)

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  /*
+    boardsService: BoardsService;
+    constructor(boardService: BoardsService) {
+    this.boardsService = boardService;
+    }
+    아래의 코드와 동일,
+    접근제한자 private 을 사용함으로써 위의 코드 3줄을 1줄로 표현 할 수 있다.
+*/
+  constructor(private boardsService: BoardsService) {}
+}
